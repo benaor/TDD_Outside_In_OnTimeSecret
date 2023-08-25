af8fbbb3c916ac35739a72cb037fe8ffa8db9141
benjamin GIRARD <benjamingirard25@gmail.com>
2023-08-25 14:56:56 +0200

    first commit

---
+++ test/GetSecrets.int.test.ts
@@ -0,0 +1,20 @@
+import supertest from "supertest";
+import server from "../src/";
+
+const request = supertest(server);
+
+describe("Get secrets integration tests", () => {
+  xit("Should return an error when the the UrlId isn't valid", async () => {
+    const response = await request.get("/api/v1/secrets/2short");
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      name: "UrlIdValidationError",
+      message: "UrlId is too short",
+    });
+  });
+
+  xit("Should return an error when the secret doesn't exist in the system", () => {});
+
+  xit("Should retrieve a secret from the system", () => {});
+});
